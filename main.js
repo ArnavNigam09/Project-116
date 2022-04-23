b0356ecb856f373404bf8ef65707a900c9db384c
ArnavNigam09 <83456570+ArnavNigam09@users.noreply.github.com>
2022-04-23 11:25:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function take_snapshot()
+{
+    save('myfilterimage.png')
+}
+
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function gotPoses(results)
+{
+    if(results.lenght>0)
+    {
+        console.log(results);
+        console.log("nose X = " + results[0].pose.nose.X);
+        console.log("nose Y = " + results[0].pose.nose.Y);
+    }
+}
+
+function modelLoaded()
+{
+    console.log('posenet is initialized');
+}
+noseX = 0;noseY = 0;
+function preload()
+{
+   clown_nose = loadImage('mustache.png')
+}
+
+function draw()
+{
+   image(video, 0, 0, 300 ,300);
+   circle(noseX,noseY,20);
+   fill(255, 0 , 0);
+   stroke(255, 0, 0);
+   image(clown_nose, noseX , noseY, 30, 30);
+}
